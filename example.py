2b9e892a02417ec0f88fdc5df7b5f4b377f945d8
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-08-02 21:42:39 +0100

    Do sampling with temperature

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 # https://huggingface.co/roneneldan/TinyStories-33M
-from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer,GenerationConfig
+
+#help(GenerationConfig)
 
 model = AutoModelForCausalLM.from_pretrained('roneneldan/TinyStories-33M')
 
@@ -9,7 +11,9 @@ prompt = "Once upon a time there was"
 
 input_ids = tokenizer.encode(prompt, return_tensors="pt")
 
-output = model.generate(input_ids, max_length = 1000, num_beams=1)
+output = model.generate(input_ids, max_length = 1000, num_beams=1,
+                        generation_config=GenerationConfig(do_sample=True,temperature=1.))
+#help(model.generate)
 
 output_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
