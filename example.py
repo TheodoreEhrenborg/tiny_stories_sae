4efedc2b8b110c5674a0f9b5ca6ae41ecb87306e
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-08-02 21:51:33 +0100

    Work on CUDA

--- example.py
+++ example.py
@@ -1,19 +1,25 @@
 # https://huggingface.co/roneneldan/TinyStories-33M
 from transformers import AutoModelForCausalLM, AutoTokenizer,GenerationConfig
 
-#help(GenerationConfig)
+# help(GenerationConfig)
 
 model = AutoModelForCausalLM.from_pretrained('roneneldan/TinyStories-33M')
+#help(model)
+
+model.cuda()
 
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
+#help(tokenizer)
 
 prompt = "Once upon a time there was"
 
-input_ids = tokenizer.encode(prompt, return_tensors="pt")
+# help(tokenizer.encode)
+input_ids = tokenizer.encode(prompt, return_tensors="pt").cuda()
+print(input_ids)
 
+# help(model.generate)
 output = model.generate(input_ids, max_length = 1000, num_beams=1,
                         generation_config=GenerationConfig(do_sample=True,temperature=1.))
-#help(model.generate)
 
 output_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
