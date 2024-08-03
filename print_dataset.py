d4852313f8111de64f1feb78c4e1982dc52a2b77
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-08-03 22:15:49 +0100

    Find-and-replace text

--- print_dataset.py
+++ print_dataset.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 from datasets import load_dataset
+
+def e(x):
+    l = x.split()
+    if "named" not in l:
+        return x
+    i = l.index("named")
+    if i+1==len(l):
+        return x
+    name = l[i+1]
+    return x.replace(name, "Einstein")
+
+
 x = load_dataset("roneneldan/TinyStories")
-print(x["train"][2])
+print(e(x["train"][2]["text"]))
 
 
 
