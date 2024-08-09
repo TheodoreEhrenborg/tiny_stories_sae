2de61c135b9cfa6d39c05fa7412b39c18c35a87a
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-08-09 11:44:45 +0000

    Reduce batch size

--- finetune.py
+++ finetune.py
@@ -35,12 +35,12 @@ from transformers import Trainer, TrainingArguments
 
 args = TrainingArguments(
     output_dir="/tmp/results",
-    per_device_train_batch_size=32,
-    per_device_eval_batch_size=32,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
     evaluation_strategy="steps",
-    eval_steps=5_000,
+    eval_steps=1,
     logging_steps=5_000,
-    gradient_accumulation_steps=8,
+    gradient_accumulation_steps=1,
     num_train_epochs=1,
     weight_decay=0.1,
     warmup_steps=1_000,
