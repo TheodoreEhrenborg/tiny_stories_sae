0ceffeb08f150131bbc3b9a49741a682c97135f8
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-08-11 21:18:55 +0100

    Add todo

--- run_with_gpus.sh
+++ run_with_gpus.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+# TODO Combine this with the other script
 docker run --gpus all -it -v $HOME/.cache/huggingface:/root/.cache/huggingface -v $(pwd):/code $(cat docker_name) /bin/bash
