fce37b06f8a7ab2b58d117d0b7bd3d2bf8c29bd7
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-08-13 08:32:15 +0100

    Remove scripts that use jupyter

--- jupyter.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-jupyter notebook --allow-root --ip 0.0.0.0 --no-browser
--- run_with_jupyter.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-docker run -p 8888:8888 --gpus all -it -v $(pwd):/code $(cat docker_name) /bin/bash
