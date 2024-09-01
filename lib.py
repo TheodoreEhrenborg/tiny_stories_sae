5fc973b2d54a88f5a68ddc8d3de213a2083c08ae
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-01 13:45:28 +0100

    Format

--- lib.py
+++ lib.py
@@ -30,6 +30,7 @@ def get_feature_vectors(
 ) -> Float[torch.Tensor, "1 seq_len {sae_hidden_dim} 768"]:
     return sae_activations.unsqueeze(3) * decoder_weight
 
+
 @jaxtyped(typechecker=beartype)
 def get_feature_magnitudes(
     sae_hidden_dim,
@@ -37,5 +38,5 @@ def get_feature_magnitudes(
     decoder_weight: Float[torch.Tensor, "{sae_hidden_dim} 768"],
 ) -> Float[torch.Tensor, "1 seq_len {sae_hidden_dim}"]:
     decoder_magnitudes = torch.linalg.vector_norm(decoder_weight, dim=1, ord=2)
-    result =  sae_activations * decoder_magnitudes
+    result = sae_activations * decoder_magnitudes
     return result
--- test.py
+++ test.py
@@ -2,20 +2,18 @@ from lib import get_feature_vectors, get_feature_magnitudes
 import torch
 
 
-
 def test_running_decoder_manually():
     sae_hidden_dim = 100
     llm_hidden_dim = 768
     seq_len = 314
     decoder = torch.nn.Linear(sae_hidden_dim, llm_hidden_dim)
-    sae_activations = torch.randn(1,seq_len, sae_hidden_dim).abs()
+    sae_activations = torch.randn(1, seq_len, sae_hidden_dim).abs()
     feat_vecs = get_feature_vectors(
         sae_hidden_dim, sae_activations, decoder.weight.transpose(0, 1)
     )
     reconstructed = torch.sum(feat_vecs, 2) + decoder.bias
-    assert torch.allclose(
-        decoder(sae_activations), reconstructed, atol=2e-5
-    )
+    assert torch.allclose(decoder(sae_activations), reconstructed, atol=2e-5)
+
 
 def test_same_loss():
     with torch.no_grad():
@@ -23,7 +21,7 @@ def test_same_loss():
         llm_hidden_dim = 768
         seq_len = 314
         decoder = torch.nn.Linear(sae_hidden_dim, llm_hidden_dim)
-        sae_activations = torch.randn(1,seq_len, sae_hidden_dim).abs()
+        sae_activations = torch.randn(1, seq_len, sae_hidden_dim).abs()
         feat_vecs = get_feature_vectors(
             sae_hidden_dim, sae_activations, decoder.weight.transpose(0, 1)
         )
