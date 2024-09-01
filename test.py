c114561408bb6d5c2cc644a456638a828338a59a
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-01 13:45:04 +0100

    Fix tests

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ def test_running_decoder_manually():
     llm_hidden_dim = 768
     seq_len = 314
     decoder = torch.nn.Linear(sae_hidden_dim, llm_hidden_dim)
-    sae_activations = torch.randn(1,seq_len, sae_hidden_dim, 1).abs()
+    sae_activations = torch.randn(1,seq_len, sae_hidden_dim).abs()
     feat_vecs = get_feature_vectors(
         sae_hidden_dim, sae_activations, decoder.weight.transpose(0, 1)
     )
     reconstructed = torch.sum(feat_vecs, 2) + decoder.bias
     assert torch.allclose(
-        decoder(sae_activations.squeeze(3)), reconstructed, atol=2e-5
+        decoder(sae_activations), reconstructed, atol=2e-5
     )
 
 def test_same_loss():
@@ -23,7 +23,7 @@ def test_same_loss():
         llm_hidden_dim = 768
         seq_len = 314
         decoder = torch.nn.Linear(sae_hidden_dim, llm_hidden_dim)
-        sae_activations = torch.randn(1,seq_len, sae_hidden_dim, 1).abs()
+        sae_activations = torch.randn(1,seq_len, sae_hidden_dim).abs()
         feat_vecs = get_feature_vectors(
             sae_hidden_dim, sae_activations, decoder.weight.transpose(0, 1)
         )
