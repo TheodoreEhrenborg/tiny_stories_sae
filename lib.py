aa70649ba9b7f5116d4c1381b3da07b3794e2a1f
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-01 10:38:23 +0100

    Get feature magnitudes using less memory, and test

--- lib.py
+++ lib.py
@@ -29,3 +29,13 @@ def get_feature_vectors(
     decoder_weight: Float[torch.Tensor, "{sae_hidden_dim} 768"],
 ) -> Float[torch.Tensor, "1 seq_len {sae_hidden_dim} 768"]:
     return sae_activations * decoder_weight
+
+@jaxtyped(typechecker=beartype)
+def get_feature_magnitudes(
+    sae_hidden_dim,
+    sae_activations: Float[torch.Tensor, "1 seq_len {sae_hidden_dim} 1"],
+    decoder_weight: Float[torch.Tensor, "{sae_hidden_dim} 768"],
+) -> Float[torch.Tensor, "1 seq_len {sae_hidden_dim}"]:
+    decoder_magnitudes = torch.linalg.vector_norm(decoder_weight, dim=1, ord=2)
+    result =  sae_activations.squeeze(3) * decoder_magnitudes
+    return result
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from lib import get_feature_vectors
+from lib import get_feature_vectors, get_feature_magnitudes
 import torch
 
 
@@ -8,7 +8,7 @@ def test_running_decoder_manually():
     llm_hidden_dim = 768
     seq_len = 314
     decoder = torch.nn.Linear(sae_hidden_dim, llm_hidden_dim)
-    sae_activations = torch.randn(1,seq_len, sae_hidden_dim, 1)
+    sae_activations = torch.randn(1,seq_len, sae_hidden_dim, 1).abs()
     feat_vecs = get_feature_vectors(
         sae_hidden_dim, sae_activations, decoder.weight.transpose(0, 1)
     )
@@ -16,3 +16,23 @@ def test_running_decoder_manually():
     assert torch.allclose(
         decoder(sae_activations.squeeze(3)), reconstructed, atol=2e-5
     )
+
+def test_same_loss():
+    with torch.no_grad():
+        sae_hidden_dim = 100
+        llm_hidden_dim = 768
+        seq_len = 314
+        decoder = torch.nn.Linear(sae_hidden_dim, llm_hidden_dim)
+        sae_activations = torch.randn(1,seq_len, sae_hidden_dim, 1).abs()
+        feat_vecs = get_feature_vectors(
+            sae_hidden_dim, sae_activations, decoder.weight.transpose(0, 1)
+        )
+        magnitudes1 = torch.linalg.vector_norm(feat_vecs, dim=3)
+        l1_1 = torch.linalg.vector_norm(magnitudes1, ord=1)
+        magnitudes2 = get_feature_magnitudes(
+            sae_hidden_dim, sae_activations, decoder.weight.transpose(0, 1)
+        )
+        l1_2 = torch.linalg.vector_norm(magnitudes2, ord=1)
+        assert magnitudes1.shape == magnitudes2.shape
+        assert torch.allclose(magnitudes1, magnitudes2)
+        assert torch.allclose(l1_1, l1_2)
