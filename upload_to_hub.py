aee470d624c3255973e766c416350251da6a9e9f
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-02 18:47:57 +0100

    Remove old scripts

--- upload_to_hub.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from transformers import AutoModelForCausalLM
-
-model = AutoModelForCausalLM.from_pretrained("/tmp/results/checkpoint-20")
-model.push_to_hub("TheodoreEhrenborg/TinyStories-33M-Einstein")
