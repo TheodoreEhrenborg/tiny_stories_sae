d771455ad87f1e6696f4a86af2f8a5798e839418
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-15 19:18:44 +0100

    Fix up uv installation

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ docker run -it --rm \
     -v $HOME/.cache/huggingface:/root/.cache/huggingface \
     -v $(pwd):/code \
     -v $HOME/.cache/uv:/root/.cache/uv \
+    -v $HOME/.local/share/uv:/root/.local/share/uv \
     $(cat docker_name) \
     sh -c "/bin/bash"
