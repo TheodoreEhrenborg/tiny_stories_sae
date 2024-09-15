39756d0acc793980bafbe32fd6e922a0ddcaf430
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-15 22:59:02 +0100

    Demo: I can print and modify activations

--- src/tiny_stories_sae/nudge.py
+++ src/tiny_stories_sae/nudge.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    GPT2TokenizerFast,
+    GPTNeoForCausalLM,
+)
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 import json
 from argparse import ArgumentParser, Namespace
 from dataclasses import asdict, dataclass
@@ -17,11 +24,56 @@ from tiny_stories_sae.lib import (
 )
 
 
+def hook_factory():
+    result_dict = {}
+
+    def hook(module, args, output):
+        print("module", module)
+        print("args", args[0].shape)
+        result_dict["output"] = output
+        print("output", output[0].shape, output[1][0].shape, output[1][1].shape)
+
+    return hook, result_dict
+
+
+def destructive_hook(module, args, output):
+    return torch.zeros_like(output[0]), output[1]
+
+
 @beartype
 def main(user_args: Namespace):
-    filtered_datasets, llm, _, tokenizer = setup(
-        user_args.sae_hidden_dim, user_args.fast
+    llm = AutoModelForCausalLM.from_pretrained("roneneldan/TinyStories-33M")
+    hook, result_dict = hook_factory()
+    llm.transformer.h[1].register_forward_hook(hook)
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
+    tokenizer.pad_token = tokenizer.eos_token
+    sample = "There once was a cat"
+    input = torch.tensor(tokenizer(sample)["input_ids"]).unsqueeze(0)
+    with torch.no_grad():
+        x = llm(input, output_hidden_states=True)
+    assert len(x.hidden_states) == 5
+    assert torch.allclose(x.hidden_states[2], result_dict["output"][0]), (
+        x.hidden_states[2] - result_dict["output"][0]
     )
+    print(tokenizer(sample, return_tensors="pt"))
+    output_text = llm.generate(
+        input,
+        max_length=100,
+        num_beams=1,
+        generation_config=GenerationConfig(do_sample=True, temperature=1.0),
+    )
+    print(tokenizer.decode(output_text[0]))
+    faulty_llm = AutoModelForCausalLM.from_pretrained("roneneldan/TinyStories-33M")
+    faulty_llm.transformer.h[1].register_forward_hook(destructive_hook)
+    faulty_output_text = faulty_llm.generate(
+        input,
+        max_length=100,
+        num_beams=1,
+        generation_config=GenerationConfig(do_sample=True, temperature=1.0),
+    )
+    print(tokenizer.decode(faulty_output_text[0]))
+    exit()
+
     with torch.no_grad():
         for step, example in enumerate(tqdm(filtered_datasets["validation"])):
             if step > user_args.max_step:
