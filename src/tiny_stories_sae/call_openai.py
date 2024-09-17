29455ccb415c7e3164faa29db9be844ddcf9c1e4
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-17 23:02:00 +0100

    Try prompting to find patterns

--- src/tiny_stories_sae/call_openai.py
+++ src/tiny_stories_sae/call_openai.py
@@ -3,28 +3,44 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 
 
-class EventDescription(BaseModel):
-    location: str
-    notable_fact: str
+class Pattern(BaseModel):
+    scratch_work: str
+    clearness: int
+    short_pattern_description: str
 
 
 load_dotenv()
 from openai import OpenAI
 
 client = OpenAI()
+import json
 
+results = json.load(
+    open("/results/enlightened-daring-angelfish-of-teaching/105000.json")
+)
+print("JSON loaded")
+
+texts = [x["annotated_text"] for x in results if x["feature_idx"] == 94]
+
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {
+        "role": "user",
+        "content": "I'll send you a sequence of texts, "
+        "where tokens have been highlighted using Unicode Block Elements. "
+        "Look for a pattern in the highlighting. "
+        "Rank how clear the pattern is on a 1-5 scale, where 1 is no detectable pattern, "
+        "3 is a vague pattern with some exceptions, and 5 is a clear pattern with no exceptions. "
+        "Focus on the pattern in the strong highlights. Describe the pattern in 10 words or less",
+    },
+] + [{"role": "user", "content": elt} for elt in texts]
+print(messages)
 response = client.beta.chat.completions.parse(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {
-            "role": "assistant",
-            "content": "The Los Angeles Dodgers won the World Series in 2020.",
-        },
-        {"role": "user", "content": "Where was it played?"},
-    ],
-    response_format=EventDescription,
+    # model="gpt-4o-2024-08-06",
+    model="gpt-4o-mini-2024-07-18",
+    messages=messages,
+    response_format=Pattern,
 )
 
 print(response)
