95713c153ddd53ccb09c9fe74ca61c57fc92b682
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-28 15:28:10 +0100

    Run linters

--- src/tiny_stories_sae/nudge.py
+++ src/tiny_stories_sae/nudge.py
@@ -8,11 +8,9 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 
 from tiny_stories_sae.lib import (
     format_token,
-    normalize_activations,
-)
-from tiny_stories_sae.lib import (
     get_llm_activation_from_tensor,
     get_rotation_between,
+    normalize_activations,
     setup,
 )
 
--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,7 +1,7 @@
 def test_imports():
-    import tiny_stories_sae.lib
     import tiny_stories_sae.call_openai
     import tiny_stories_sae.gather_high_activations
     import tiny_stories_sae.gather_high_activations_llm
+    import tiny_stories_sae.lib
     import tiny_stories_sae.nudge
-    import tiny_stories_sae.train_sae
+    import tiny_stories_sae.train_sae  # noqa: F401
