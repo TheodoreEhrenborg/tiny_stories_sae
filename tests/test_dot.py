c1c990bacdf6069e00ae82106a872bae1551f439
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-28 16:16:08 +0100

    Add two docstrings

--- tests/test_dot.py
+++ tests/test_dot.py
@@ -5,6 +5,7 @@ from tiny_stories_sae.common.angle import get_rotation_between
 
 
 def test_rotation():
+    """Sanity checks on 2d vectors"""
     x = torch.tensor([1, 1])
     y = torch.tensor([1, 1])
     assert get_rotation_between(x, y) == 0
@@ -15,7 +16,7 @@ def test_rotation():
 
 
 def test_random():
-    # Random high-dimensional vectors should be nearly orthogonal
+    """Random high-dimensional vectors should be nearly orthogonal"""
     for _ in range(10):
         x = torch.randn(768)
         y = torch.randn(768)
