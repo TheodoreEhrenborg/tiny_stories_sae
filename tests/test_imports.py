d60693291afdb4f8d3513137ba10baf87bed9bfa
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-28 16:07:13 +0100

    Remove empty file

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -2,6 +2,5 @@ def test_imports():
     import tiny_stories_sae.call_openai
     import tiny_stories_sae.gather_high_activations
     import tiny_stories_sae.gather_high_activations_llm
-    import tiny_stories_sae.lib
     import tiny_stories_sae.nudge
     import tiny_stories_sae.train_sae  # noqa: F401
