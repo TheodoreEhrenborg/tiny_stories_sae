eb9ca1765bb16b946424e8da0452b835b466670f
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-10-01 22:15:26 +0100

    Set title

--- src/tiny_stories_sae/plot.py
+++ src/tiny_stories_sae/plot.py
@@ -8,6 +8,9 @@ sns.set_theme()
 data = json.load(open("/results/gpt4_api/20240930-195450llm_abs_full"))
 df = pandas.DataFrame(data["responses"])
 seaborn_plot = sns.countplot(df, x="clearness")
-seaborn_plot.set(xlabel="GPT-4's ranking of clearness")
+seaborn_plot.set(
+    xlabel="GPT-4's ranking of clearness",
+    title="LLM activations, put through absolute value",
+)
 fig = seaborn_plot.get_figure()
 fig.savefig("/results/out.png")
