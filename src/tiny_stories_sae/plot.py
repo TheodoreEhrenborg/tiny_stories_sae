2bb83a301e1405f5bd81a4065c02c1c5e5e39169
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-10-02 18:10:37 +0100

    Put inside function

--- src/tiny_stories_sae/plot.py
+++ src/tiny_stories_sae/plot.py
@@ -3,14 +3,20 @@ import json
 import pandas
 import seaborn as sns
 
-sns.set_theme()
 
-data = json.load(open("/results/gpt4_api/20240930-195450llm_abs_full"))
-df = pandas.DataFrame(data["responses"])
-seaborn_plot = sns.countplot(df, x="clearness")
-seaborn_plot.set(
-    xlabel="GPT-4's ranking of clearness",
-    title="LLM activations, put through absolute value",
-)
-fig = seaborn_plot.get_figure()
-fig.savefig("/results/out.png")
+def main():
+    sns.set_theme()
+
+    data = json.load(open("/results/gpt4_api/20240930-195450llm_abs_full"))
+    df = pandas.DataFrame(data["responses"])
+    seaborn_plot = sns.countplot(df, x="clearness")
+    seaborn_plot.set(
+        xlabel="GPT-4's ranking of clearness",
+        title="LLM activations, put through absolute value",
+    )
+    fig = seaborn_plot.get_figure()
+    fig.savefig("/results/out.png")
+
+
+if __name__ == "__main__":
+    main()
