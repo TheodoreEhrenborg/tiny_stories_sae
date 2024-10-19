15c1be5b843cb7ae8b494e5dbcdbdbdec888f541
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-10-19 16:02:19 +0100

    First try at new plotting script

---
+++ src/tiny_stories_sae/combined_plot.py
@@ -0,0 +1,51 @@
+import json
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from collections import Counter
+
+import pandas as pd
+import seaborn as sns
+from beartype import beartype
+
+
+@beartype
+def make_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument("--response_jsons", type=Path, required=True, nargs="+")
+    parser.add_argument("--labels", type=str, required=True, nargs="+")
+    parser.add_argument("--output_png", type=str, default="/results/out.png")
+    parser.add_argument("--xlabel", type=str)
+    parser.add_argument("--title", type=str)
+    return parser
+
+
+@beartype
+def get_dataframe(path: Path, label: str) -> pd.DataFrame:
+    data = json.load(path.open())
+    assert data["model"] == "gpt-4o-2024-08-06", "Don't use results from GPT-4o mini"
+
+    frame = pd.DataFrame(data["responses"])
+    frame["label"] = label
+    return frame
+
+
+def main(args: Namespace):
+    sns.set_theme()
+    frames = [
+        get_dataframe(path, label)
+        for path, label in zip(args.response_jsons, args.labels, strict=True)
+    ]
+    df = pd.concat(frames)
+
+    seaborn_plot = sns.countplot(df, x="clearness")
+    if args.xlabel is not None:
+        seaborn_plot.set(xlabel=args.xlabel)
+    if args.title is not None:
+        seaborn_plot.set(title=args.title)
+    fig = seaborn_plot.get_figure()
+    print(f"Writing to {args.output_png}")
+    fig.savefig(args.output_png)
+
+
+if __name__ == "__main__":
+    main(make_parser().parse_args())
